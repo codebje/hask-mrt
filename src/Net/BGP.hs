bb93cc68e35abf7e48985d79bf27bf7bb4de44a1
Byron Ellacott <bje@the-wanderers.org>
2015-09-29 20:55:06 +1000

    Add a useless test

---
+++ src/Net/BGP.hs
@@ -0,0 +1,2 @@
+module Net.BGP where
+
--- src/Net/MRT.hs
+++ src/Net/MRT.hs
@@ -7,7 +7,7 @@ Stability   : Experimental
 MRT is a library for parsing Multi-Threaded Routing Toolkit (MRT) export
 files, of the kind you might find on the RouteViews archive.
 -}
-module MRT
+module Net.MRT
     ( Timestamp
     , ASNumber
     , MRTMessage
