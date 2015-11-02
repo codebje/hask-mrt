c7494daade4053f85274d89a5e70d3a2e4bda193
Byron Ellacott <code@bje.id.au>
2015-11-02 17:35:45 +1000

    Start in on pretty printing

--- test/Main.hs
+++ test/Main.hs
@@ -9,6 +9,7 @@ files, of the kind you might find on the RouteViews archive.
 -}
 module Main where
 
+import Data.Maybe (listToMaybe)
 import qualified Codec.Compression.BZip as BZ
 import           Control.Monad          (liftM)
 import           Data.Binary.MRT
@@ -18,7 +19,20 @@ import           System.Environment
 loadStream :: FilePath -> IO BL.ByteString
 loadStream = liftM BZ.decompress . BL.readFile
 
+printRib :: IPRange -> RIBEntry -> IO ()
+printRib ip rib = do
+    let path = listToMaybe $ filter isPath $ getBGPAttributes rib
+    putStrLn $ foldl (++) "" [show ip, "|", show path]
+    --let attrs = filter (getBGPAttributes rib
+  where
+    isPath x = case x of {ASPath _ -> True; _ -> False}
+
+printEntry :: MRTMessage -> IO ()
+printEntry msg = case getRecord msg of
+    (TableDumpV2 _ ip ribs) -> mapM_ (printRib ip) ribs
+    _                       -> return ()
+
 main :: IO ()
 main = do
   args <- getArgs
-  mapM_ ((mapM_ print =<<) . (liftM readMessages <$> loadStream)) args
+  mapM_ ((mapM_ printEntry =<<) . (liftM readMessages <$> loadStream)) args
